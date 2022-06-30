315fbfa7e5ee2c65fe4ff2e38b2c86151c26f49e
Viacheslav <vaceslavkozin619@gmail.com>
2022-06-30 17:48:19 +0200

    XBPS package creation script fixed

--- packaging/xbps/xbps-create.sh
+++ packaging/xbps/xbps-create.sh
@@ -1,1 +1,17 @@
-xbps-create -A x86_64 -n spm-1.0_3 -D "git>=2.9.5 xdg-utils>=1.0.0 libbsd" -H "https://github.com/slamko/spm" -s "Suckless Patch Manager" --compression=xz "$1"
+#!/bin/sh
+
+# arg $1 - target directory
+# arg $2 - architecture
+
+xbps-create --architecture "$2" \
+			--pkgver spm-1.0_3 \
+			--dependencies "git>=2.9.5 xdg-utils>=1.0.0 libbsd>=0.2.1" \
+			--homepage "https://github.com/slamko/spm" \
+			--desc "Suckless Patch Manager" \
+			--compression=xz \
+			--license GPLv3 \
+			--maintainer "Viacheslav <https://github.com/slamko>" \
+			--long-desc "Command line tool for easy patch managing for suckless software." \
+			"$1"
+			
+
